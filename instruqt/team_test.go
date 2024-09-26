f0e281bd7ca55367b61b15b2c0ae405bd765f146
Raphaël Pinson <raphael@isovalent.com>
2024-09-26 22:52:58 +0200

    Add EncryptPII()
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- instruqt/team_test.go
+++ instruqt/team_test.go
@@ -15,6 +15,11 @@
 package instruqt
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"net/url"
 	"testing"
 
 	"github.com/shurcooL/graphql"
@@ -39,4 +44,41 @@ func TestGetTPGPublicKey(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, expectedPublicKey, publicKey)
 	mockClient.AssertExpectations(t)
+}
+
+func TestEncryptPII(t *testing.T) {
+	// Generate a temporary RSA key pair for testing
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.NoError(t, err)
+
+	// Extract the public key and encode it in PEM format
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	assert.NoError(t, err)
+	publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	})
+
+	// Create the mock client
+	mockClient := new(MockGraphQLClient)
+	client := &Client{
+		GraphQLClient: mockClient,
+	}
+	mockClient.On("Query", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		query := args.Get(1).(*teamQuery)
+		query.Team.TPGPublicKey = graphql.String(publicKeyPEM)
+	}).Return(nil)
+
+	// Create the PII data to be encrypted
+	data := url.Values{}
+	data.Set("email", "test@example.com")
+	data.Set("first_name", "John")
+	data.Set("last_name", "Doe")
+
+	// Call the EncryptPII function
+	encryptedPII, err := client.EncryptPII(data.Encode())
+	assert.NoError(t, err)
+
+	// Ensure the encrypted PII is a non-empty string
+	assert.NotEmpty(t, encryptedPII)
 }
